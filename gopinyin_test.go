e71f828368a79c3a88a31aff9e71b3a02170b696
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2016-06-25 13:59:04 +0800

    add Abbreviate and Value for Pinyins

--- gopinyin_test.go
+++ gopinyin_test.go
@@ -8,6 +8,10 @@ import (
 	"github.com/caiguanhao/gopinyin"
 )
 
+func get1st(first interface{}, args ...interface{}) interface{} {
+	return first
+}
+
 func assert(t *testing.T, actual, expected interface{}) {
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("%s should equal to %s", actual, expected)
@@ -22,18 +26,29 @@ func TestSplit(t *testing.T) {
 	assert(t, gopinyin.Split("zhongguo"), gopinyin.Pinyins{"zhong", "guo"})
 }
 
+func TestAbbr(t *testing.T) {
+	assert(t, gopinyin.Split("zhoguo").Abbreviate(), gopinyin.Pinyins{"z", "g"})
+}
+
 func TestExpand(t *testing.T) {
 	assert(t, gopinyin.Split("zhoguo").Expand(), gopinyin.Pinyins{"zhong,zhou", "guo"})
 }
 
 func TestSQL(t *testing.T) {
-	assert(t, gopinyin.Split("caiguanhao").Expand().SQL("pinyin"), "pinyin && '{cai}' AND pinyin && '{guan,guang}' AND pinyin && '{hao}'")
+	assert(t, gopinyin.Split("caiguanhao").Expand().SQL("pinyin"), "SEQUENCED_ARRAY_CONTAINS(pinyin, '{cai}', '{guan,guang}', '{hao}')")
 	assert(t, gopinyin.Split("").Expand().SQL("pinyin"), "")
 	assert(t, gopinyin.Split("123").Expand().SQL("pinyin"), "")
+}
+
+func TestValue(t *testing.T) {
+	assert(t, get1st(gopinyin.Pinyins{}.Value()), "{}")
+	assert(t, get1st(gopinyin.Pinyins{"0", "z", "1", "g", "2"}.Value()), "{z,g}")
+	assert(t, get1st(gopinyin.Pinyins{"zhong", "guo"}.Value()), "{zhong,guo}")
+	assert(t, get1st(gopinyin.Split("zg").Value()), "{z,g}")
 }
 
 func Example_postgreSQL() {
 	fmt.Printf("SELECT * FROM data WHERE %s\n", gopinyin.Split("shouj").Expand().SQL("pinyin"))
 	// Output:
-	// SELECT * FROM data WHERE pinyin && '{shou}' AND pinyin && '{ji,jia,jian,jiang,jiao,jie,jin,jing,jiong,jiu,ju,juan,jue,jun}'
+	// SELECT * FROM data WHERE SEQUENCED_ARRAY_CONTAINS(pinyin, '{shou}', '{ji,jia,jian,jiang,jiao,jie,jin,jing,jiong,jiu,ju,juan,jue,jun}')
 }
